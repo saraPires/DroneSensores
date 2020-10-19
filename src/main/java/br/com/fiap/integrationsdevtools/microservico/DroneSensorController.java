c477ab69ca13d1fa7009de5bcc38d081a099355a
saraPiresDev <sararegina.pires@gmail.com>
2020-10-19 19:04:28 -0300

    Classe controller receber dados da página

---
+++ src/main/java/br/com/fiap/integrationsdevtools/microservico/DroneSensorController.java
@@ -0,0 +1,9 @@
+package br.com.fiap.integrationsdevtools.microservico;
+
+public class DroneSensorController {
+
+	public DroneSensorController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
