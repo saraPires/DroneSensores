316140b020296edd83def07116fab5a3fd146709
saraPiresDev <sararegina.pires@gmail.com>
2020-10-19 19:26:10 -0300

    POJO Drone..

--- src/main/java/br/com/fiap/integrationsdevtools/dto/DroneDTO.java
+++ src/main/java/br/com/fiap/integrationsdevtools/dto/DroneDTO.java
@@ -2,8 +2,38 @@ package br.com.fiap.integrationsdevtools.dto;
 
 public class DroneDTO {
 
-	public DroneDTO() {
-		// TODO Auto-generated constructor stub
+	private Long droneid;
+	private Long latitude;
+	private Long logitude;
+	private Long temperatura;
+	
+	
+	public Long getDroneid() {
+		return droneid;
 	}
+	public void setDroneid(Long droneid) {
+		this.droneid = droneid;
+	}
+	public Long getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Long latitude) {
+		this.latitude = latitude;
+	}
+	public Long getLogitude() {
+		return logitude;
+	}
+	public void setLogitude(Long logitude) {
+		this.logitude = logitude;
+	}
+	public Long getTemperatura() {
+		return temperatura;
+	}
+	public void setTemperatura(Long temperatura) {
+		this.temperatura = temperatura;
+	}
+	
+	
+	
 
 }
--- src/main/java/br/com/fiap/integrationsdevtools/service/DroneSensorService.java
+++ src/main/java/br/com/fiap/integrationsdevtools/service/DroneSensorService.java
@@ -2,8 +2,6 @@ package br.com.fiap.integrationsdevtools.service;
 
 public class DroneSensorService {
 
-	public DroneSensorService() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 }
