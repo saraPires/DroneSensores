14b7981a28b5ccc29ef7e4f05a7998309af09920
saraPiresDev <sararegina.pires@gmail.com>
2020-10-19 19:01:07 -0300

    Commit projeto - mircoserviços

---
+++ src/main/java/br/com/fiap/integrationsdevtools/DroneSensoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.integrationsdevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneSensoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneSensoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/integrationsdevtools/DroneSensoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.integrationsdevtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneSensoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
