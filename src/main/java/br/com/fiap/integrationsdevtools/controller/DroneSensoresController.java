d985fe1239ae64db95e5931cf1d67a4585e771f8
saraPiresDev <sararegina.pires@gmail.com>
2020-10-19 19:42:58 -0300

    Criando post request

--- src/main/java/br/com/fiap/integrationsdevtools/controller/DroneSensoresController.java
+++ src/main/java/br/com/fiap/integrationsdevtools/controller/DroneSensoresController.java
@@ -1,9 +1,26 @@
 package br.com.fiap.integrationsdevtools.controller;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.integrationsdevtools.service.DroneService;
+
+
+
+@RestController
+@RequestMapping("rest")
 public class DroneSensoresController {
 
-	public DroneSensoresController() {
-		// TODO Auto-generated constructor stub
-	}
+    private final Logger logger = LoggerFactory.getLogger(DroneSensoresController.class);
 
+    private final DroneService droneService;
+
+    public DroneSensoresController(DroneService droneService) {
+        this.droneService = droneService;
+    }
+
+	
+	public RequestBody<> envioDadosDroneSensor()
+	
 }
---
+++ src/main/java/br/com/fiap/integrationsdevtools/service/DroneService.java
@@ -0,0 +1,5 @@
+package br.com.fiap.integrationsdevtools.service;
+
+public class DroneService {
+
+}
