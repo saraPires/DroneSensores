415b9ee4c2dd0a8603a5a0297a3ab9c0f1fd292a
saraPiresDev <sararegina.pires@gmail.com>
2020-10-24 21:20:41 -0300

    Commit configurações do envio de mensagens..

---
+++ src/main/java/br/com/fiap/integrationmicroservice/SenderAppConfig.java
@@ -0,0 +1,12 @@
+package br.com.fiap.integrationmicroservice;
+
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SenderAppConfig {
+	
+
+}
--- src/main/java/br/com/fiap/integrationmicroservice/consumer/DroneConsumer.java
+++ src/main/java/br/com/fiap/integrationmicroservice/consumer/DroneConsumer.java
@@ -1,28 +1,46 @@
 package br.com.fiap.integrationmicroservice.consumer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.fiap.integrationmicroservice.configuration.Configuracao;
 import br.com.fiap.integrationmicroservice.dto.DroneDTO;
 
 
 public class DroneConsumer {
-
 	
-	public void receiveDrone (DroneDTO dronDTO) {
+    @Value("${queue.order.name}")
+    private String orderQueue;
+	
+	public List<DroneDTO> receiveDrone() throws JsonParseException, JsonMappingException, IOException {
 		
+		List<DroneDTO> dadosDrone = new ArrayList<>();
         RabbitTemplate template = new RabbitTemplate(Configuracao.getConnection());
 
         while(true){
             try {
-                byte[] body = template.receive("drone-queue.entrada").getBody();
-                System.out.println(new String(body));
+                byte[] body = template.receive(orderQueue).getBody();
+                //System.out.println(new String(body));
+                
+                DroneDTO message = new ObjectMapper().readValue(body, DroneDTO.class);
+                
+                dadosDrone.add(message);
             } catch (NullPointerException ex){
                 System.out.println("fila vazia!");
                 break;
             }
         }
-
+        return dadosDrone;
     }
 		
 	
