009046610bd8841fdb83f1a9257d2f58c0b0753e
saraPiresDev <sararegina.pires@gmail.com>
2020-10-26 18:49:54 -0300

    Reirando os métodos de chamada no banco.

--- src/main/java/br/com/fiap/integrationmicroservice/produtor/DroneProdutor.java
+++ src/main/java/br/com/fiap/integrationmicroservice/produtor/DroneProdutor.java
@@ -20,25 +20,14 @@ public class DroneProdutor {
 
     @Value("${queue.order.name}")
     private String orderQueue;
-
+    
     
     public void sendDrone( DroneCreateDTO droneCreateDTO ) throws Exception {
     	
     	//Convertendo o objeto para Json
     	Gson gson = new Gson();
     	String dadosDrone = gson.toJson( droneCreateDTO );
-    	
-        //Set up queue, exchanges and bindings
-        RabbitAdmin admin = new RabbitAdmin(Configuracao.getConnection());
-        Queue queueDrone = new Queue(orderQueue);
         final String exchange = "drone.entrada";
-        admin.declareQueue(queueDrone);
-		/*
-		 * DirectExchange exchangeDrone = new DirectExchange(exchange);
-		 * admin.declareExchange(exchangeDrone);
-		 * admin.declareBinding(BindingBuilder.bind(queueDrone).to(exchangeDrone).with(
-		 * dadosDrone));
-		 */
         RabbitTemplate template = new RabbitTemplate(Configuracao.getConnection());
         template.convertAndSend(exchange, "drone_data", dadosDrone );
     }
--- src/main/java/br/com/fiap/integrationmicroservice/service/DroneService.java
+++ src/main/java/br/com/fiap/integrationmicroservice/service/DroneService.java
@@ -1,28 +1,12 @@
 package br.com.fiap.integrationmicroservice.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import br.com.fiap.integrationmicroservice.dto.DroneCreateDTO;
-import br.com.fiap.integrationmicroservice.dto.DroneCreateUpdateDTO;
-import br.com.fiap.integrationmicroservice.dto.DroneDTO;
-import br.com.fiap.integrationmicroservice.dto.DroneMedicoesDTO;
-
-
 
 
 @Service
 public interface DroneService  {
 	
 	 void send(DroneCreateDTO droneCreateDTO);
-	 List<DroneDTO> getAllDrone();
-	 DroneDTO getMonitorDroneById(DroneDTO droneid);
-	 List<DroneMedicoesDTO> getAllDrones();
-	 DroneMedicoesDTO getAllMonitoramentoByDroneId(Long id);
-	 void delete(Long id);
-	 List<DroneDTO> findAll(Integer numero);
-	 DroneDTO update(Long id, DroneCreateUpdateDTO productCreateUpdateDTO);
-	 DroneDTO findById(Long id); 
 	
 }
