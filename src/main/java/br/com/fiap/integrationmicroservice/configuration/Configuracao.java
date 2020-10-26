b7f63fe74a0f2c9abd7df6fed147658fda1c52a6
saraPiresDev <sararegina.pires@gmail.com>
2020-10-26 19:27:37 -0300

    Alterando as configurações para arquivo properties

--- src/main/java/br/com/fiap/integrationmicroservice/configuration/Configuracao.java
+++ src/main/java/br/com/fiap/integrationmicroservice/configuration/Configuracao.java
@@ -1,21 +1,24 @@
 package br.com.fiap.integrationmicroservice.configuration;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.core.env.Environment;
+import java.util.Properties;
 
 public class Configuracao {
-    private static CachingConnectionFactory connectionFactory;
+	private static CachingConnectionFactory connectionFactory;
+	private static Environment environment;
 
     public static CachingConnectionFactory getConnection(){
-
+    	
         if(connectionFactory == null){
-            connectionFactory = new CachingConnectionFactory("rattlesnake.rmq.cloudamqp.com");//TODO add hostname
-            connectionFactory.setUsername("xcqnwfuc");//TODO add username
-            connectionFactory.setPassword("j8SOCFBdHduGTUHpzAXed2w5hun8tLA9");//TODO add password
-            connectionFactory.setVirtualHost("xcqnwfuc");//TODO add virtualhost
+            connectionFactory = new CachingConnectionFactory(environment.getProperty("spring.rabbitmq.host"));//TODO add hostname
+            connectionFactory.setUsername(environment.getProperty("spring.rabbitmq.username"));//TODO add username
+            connectionFactory.setPassword(environment.getProperty("spring.rabbitmq.password"));//TODO add password
+            connectionFactory.setVirtualHost(environment.getProperty("spring.rabbitmq.virtual-host"));//TODO add virtualhost
 
             //Recommended settings
-            connectionFactory.setRequestedHeartBeat(30);
-            connectionFactory.setConnectionTimeout(30000);
+            connectionFactory.setRequestedHeartBeat(Integer.valueOf(environment.getProperty("spring.rabbitmq.requested-heartbeat")));
+            connectionFactory.setConnectionTimeout(Integer.valueOf(environment.getProperty("spring.rabbitmq.connection-timeout")));
         }
 
         return connectionFactory;
